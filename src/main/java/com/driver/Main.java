7b6180286f36b44cd743032754e813caea94b9b9
Sachchida99 <bhardwajsachchidanand6@gmail.com>
2024-03-16 18:24:24 +0530

    Initial Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        // Attempting to directly access private member variable "name"
+        // Results in compilation error due to access restriction
+        // obj.name = "John"; // Error: The field RWOnly.name is not visible
+
+        // Attempting to print private member variable "name"
+        // Also results in compilation error due to access restriction
+        // System.out.println(obj.name); // Error: The field RWOnly.name is not visible
+
+        // Setting value to "name" using setter function
+        obj.setName("John");
+
+        // Accessing value of "name" using getter function
+        System.out.println("Name: " + obj.getName()); // Output: Name: John
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Getter function for "name"
+    public String getName() {
+        return name;
+    }
+
+    // Setter function for "name"
+    public void setName(String name) {
+        this.name = name;
+    }
 }
